309c13b708cdca44355e7aa6fc8921094d07947e
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-10-18 10:08:33 +0200

    ...

--- maybe-gauss-elim/main.py
+++ maybe-gauss-elim/main.py
@@ -23,28 +23,28 @@ def pretty_print(m):
         for col in row:
             print(col, '\t', end='')
         print()
-    
+
     print()
 
 
 def pivot(m, k):
-
     """
     To avoid having a r1[k] == 0 and doing: alpha = r2[k]/0.
     """
 
     # list of tuples, each tuple has row-index and row
-    rows = list(enumerate(m))  
+    rows = list(enumerate(m))
 
     # get index i of row with max k-th element
     # TODO: limit the number of rows to the ones that still have to be eliminated plus the one above them, depends on k
-    i = max(rows,  key=lambda r :  (-1 if r[0] > k else 1) * abs(r[1][k])   ) [0]
+    i = max(rows,  key=lambda r:  (-1 if r[0] > k else 1) * abs(r[1][k]))[0]
 
-    m2 = m[:] # copy
+    m2 = m[:]  # copy
 
     # swap rows such that row i goes at the top
     m2[0], m2[i] = m2[i], m2[0]
     return m2
+
 
 def test_pivot():
 
@@ -53,39 +53,46 @@ def test_pivot():
          [2, 6, 7, 0]]
 
     return (
-     pivot(m, 3) == [[1, 4, 3, 6], [3, 6, 3, 0], [2, 6, 7, 0]] and 
-     pivot(m, 2) == [[2, 6, 7, 0], [1, 4, 3, 6], [3, 6, 3, 0]] and 
-     pivot(m, 1) == [[3, 6, 3, 0], [1, 4, 3, 6], [2, 6, 7, 0]])
-
+        pivot(m, 3) == [[1, 4, 3, 6], [3, 6, 3, 0], [2, 6, 7, 0]] and
+        pivot(m, 2) == [[2, 6, 7, 0], [1, 4, 3, 6], [3, 6, 3, 0]] and
+        pivot(m, 1) == [[3, 6, 3, 0], [1, 4, 3, 6], [2, 6, 7, 0]])
 
 
 def gem(m):
 
     n = len(m)
 
-    for k in range(n): # for each possible column k
+    for k in range(n):  # for each possible column k
 
-        m = pivot(m, k) # pivoting step
-        r1 = k # row one, used for simplifying
+        m = pivot(m, k)  # pivoting step
+        r1 = k  # row one, used for simplifying
 
-        for r2 in range(r1+1, n):# for each possible row two, below row one
+        for r2 in range(r1+1, n):  # for each possible row two, below row one
 
-            print("for rows:",r1, r2, "on col:", k, "\n")
+            print("for rows:", r1, r2, "on col:", k, "\n")
             m = apply(m, r1, r2, k)
             pretty_print(m)
 
     return m
 
 
-m = [
-    [3, 6, 3, 0],
-    [1, 4, 3, 6],
-    [2, 6, 7, 0],
-]
+if __name__ == '__main__':
 
-# assert test_pivot()
-print("initial:")
-pretty_print(m)
-m = gem(m)
-print("final:")
-pretty_print(m)
+    # m = [
+    #     [3, 6, 3, 0],
+    #     [1, 4, 3, 6],
+    #     [2, 6, 7, 0],
+    # ]
+
+    m = [
+        [2, 7, 5],
+        [14, 50, 36],
+        [2, 8, 8],
+    ]
+
+    assert test_pivot()
+    print("initial:")
+    pretty_print(m)
+    m = gem(m)
+    print("final:")
+    pretty_print(m)
